2526f51697416b0f5d522de2126c83be30d78e45
xc <3152305245@qq.com>
2019-07-03 16:33:06 +0800

    test 分支

---
+++ ddiancan/src/main/java/com/ecpss/DdiancanApplication.java
@@ -0,0 +1,19 @@
+package com.ecpss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdiancanApplication {
+	private Logger logger= LoggerFactory.getLogger(DdiancanApplication.class);
+
+	public static void main(String[] args) {
+//		测试合并分支
+//		好棒
+		
+		SpringApplication.run(DdiancanApplication.class, args);
+	}
+
+}
