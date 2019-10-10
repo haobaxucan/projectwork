55bdda782e9d8fdea69a37a43c2e78c8af335602
your name <your email>
2019-10-10 17:29:53 +0800

    coding

---
+++ wx_public_number/src/main/java/com/ecpss/wx_public_number/WxPublicNumberApplication.java
@@ -0,0 +1,14 @@
+package com.ecpss.wx_public_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPublicNumberApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WxPublicNumberApplication.class, args);
+    }
+
+}
